a150dd00e2bcde1b2ef9ce2547bedc63523b2523
Janice <github.snugly121@passmail.net>
2025-05-02 14:58:37 +0800

    Add sitemap, postcss, autoprefixer and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://jinjiaho.github.io/7-wonders-helper",
+  integrations: [sitemap()],
+  server: {
+    headers: {
+      "Content-Security-Policy":
+        "default-src 'self'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline';",
+    },
+  },
+});
