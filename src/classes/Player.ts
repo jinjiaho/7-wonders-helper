a38980c737fb586e1dcbe68e6452af37efadae2b
Janice <github.snugly121@passmail.net>
2025-05-02 15:32:28 +0800

    Create Player class

---
+++ src/classes/Player.ts
@@ -0,0 +1,44 @@
+interface VPComponents {
+  wonder: number;
+  treasure: number;
+  military: number;
+  blueCards: number;
+  yellowCards: number;
+  greenCards: number;
+  purpleCards: number;
+}
+
+export default class Player {
+  name: string;
+  vpComponents: VPComponents = {
+    wonder: 0,
+    treasure: 0,
+    military: 0,
+    blueCards: 0,
+    yellowCards: 0,
+    greenCards: 0,
+    purpleCards: 0,
+  };
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  setVPComponent(key: keyof VPComponents, pts: number) {
+    this.vpComponents[key] = pts;
+  }
+
+  getVPComponent(key: keyof VPComponents) {
+    return this.vpComponents[key];
+  }
+
+  getTotalVP() {
+    return Object.values(this.vpComponents).reduce((sum, component) => {
+      return sum + component;
+    }, 0);
+  }
+}
